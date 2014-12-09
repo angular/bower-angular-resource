2cf92c513ad7a98dbac94f077142d0a7275e06de
jenkins <ci-bot@angularjs.org>
2014-12-09 10:02:56 -0800

    v1.3.7-build.3686+sha.3b5ba87

--- angular-resource.js
+++ angular-resource.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.7-build.3684+sha.a808916
+ * @license AngularJS v1.3.7-build.3686+sha.3b5ba87
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -116,7 +116,7 @@ function shallowClearAndCopy(src, dst) {
  *   example, if the `defaultParam` object is `{someParam: '@someProp'}` then the value of `someParam`
  *   will be `data.someProp`.
  *
- * @param {Object.<Object>=} actions Hash with declaration of custom action that should extend
+ * @param {Object.<Object>=} actions Hash with declaration of custom actions that should extend
  *   the default set of resource actions. The declaration should be created in the format of {@link
  *   ng.$http#usage $http.config}:
  *
