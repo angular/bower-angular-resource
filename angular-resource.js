f2391574888cf9d5b519d2b20854d0236d170430
jenkins <ci-bot@angularjs.org>
2015-02-03 13:32:57 -0800

    v1.4.0-beta.3

--- angular-resource.js
+++ angular-resource.js
@@ -1,6 +1,6 @@
 /**
- * @license AngularJS v1.3.12-build.33+sha.d1b6480
- * (c) 2010-2014 Google, Inc. http://angularjs.org
+ * @license AngularJS v1.4.0-beta.3
+ * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
 (function(window, angular, undefined) {'use strict';
