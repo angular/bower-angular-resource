56e2c68af2a9559ae351e7a6c26b6100b90741a9
jenkins <ci-bot@angularjs.org>
2015-02-22 13:27:32 -0800

    v1.4.0-build.3853+sha.f6d0ac5

--- angular-resource.js
+++ angular-resource.js
@@ -1,6 +1,6 @@
 /**
- * @license AngularJS v1.3.14-build.52+sha.0689c36
- * (c) 2010-2014 Google, Inc. http://angularjs.org
+ * @license AngularJS v1.4.0-build.3853+sha.f6d0ac5
+ * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
 (function(window, angular, undefined) {'use strict';
@@ -90,7 +90,7 @@ function shallowClearAndCopy(src, dst) {
      }]);
  * ```
  *
- * @param {string} url A parametrized URL template with parameters prefixed by `:` as in
+ * @param {string} url A parameterized URL template with parameters prefixed by `:` as in
  *   `/user/:username`. If you are using a URL with a port number (e.g.
  *   `http://example.com:8080/api`), it will be respected.
  *
