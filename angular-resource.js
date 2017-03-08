356d1c08812c4e95a0efdf8ffd9b5dc6fe008b27
jenkins <ci-bot@angularjs.org>
2017-03-08 05:55:02 -0800

    v1.6.4-build.5311+sha.1daa4f2

--- angular-resource.js
+++ angular-resource.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.3
+ * @license AngularJS v1.6.4-build.5311+sha.1daa4f2
  * (c) 2010-2017 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -283,7 +283,7 @@ function shallowClearAndCopy(src, dst) {
  *
  *   - `toJSON`: It returns a simple object without any of the extra properties added as part of
  *     the Resource API. This object can be serialized through {@link angular.toJson} safely
- *     without attaching Angular-specific fields. Notice that `JSON.stringify` (and
+ *     without attaching AngularJS-specific fields. Notice that `JSON.stringify` (and
  *     `angular.toJson`) automatically use this method when serializing a Resource instance
  *     (see [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify#toJSON()_behavior)).
  *
@@ -434,7 +434,7 @@ function shallowClearAndCopy(src, dst) {
  *
  */
 angular.module('ngResource', ['ng']).
-  info({ angularVersion: '1.6.3' }).
+  info({ angularVersion: '1.6.4-build.5311+sha.1daa4f2' }).
   provider('$resource', function ResourceProvider() {
     var PROTOCOL_AND_IPV6_REGEX = /^https?:\/\/\[[^\]]*][^/]*/;
 
